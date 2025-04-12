a1e53a8196ea65bd904d687aa1b40c5085637d71
lightsspeed <akhileshias91@gmail.com>
2025-04-12 16:19:02 +0530

    changes to provider.tf

--- provider.tf
+++ provider.tf
@@ -7,10 +7,11 @@ terraform {
     }
   }
   backend "s3" {
-    bucket       = "portfolio-terraform-state-april-2025"
-    key          = "state.tfstate"
-    region       = "ap-south-1"
-    use_lockfile = true
+    bucket = "portfolio-terraform-state-april-2025"
+    key    = "state.tfstate"
+    region = "ap-south-1"
+    # use_lockfile = true
+    dynamodb_table = "portfolio-lock"
   }
 }
 
